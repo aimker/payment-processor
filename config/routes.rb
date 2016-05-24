a64140f125b96eacc547cce3d77ebe47d0646282
Aimilios Kerasidis <aimilios.kerasidis@gmail.com>
2016-05-24 08:08:36 +0300

    Refined and fixed dynamic product purchasing

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  root 'pages#home'
+  root 'products#index'
 
   # Support Stripe payments through charges
   resources :charges
 
   # Route for showing products
-  resources :products, only: [:index, :show]
+  resources :products, only: [:show]
 
   # Route for showing purchases
   resources :purchases, only: [:show]
